7e20646145a4300aaf65b5f236228c7b149b6a64
alexisligon <aligo@macbook-pro.home>
2021-07-07 13:33:18 -0400

    get all users and get one user route added

--- controllers/user-route.js
+++ controllers/user-route.js
@@ -1,23 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+const user = require('../models/user');
+
 const router = require('express').Router();
 
-const users = [
-  {
-    firstName: 'Alexis',
-    fave_book: 'Norwegian Wood'
-  },
-  {
-    firstName: 'Naomi',
-    fave_book: 'Where The Red Fern Grows'
-  }
-];
 // get all users
 router.get('/', async (req, res) => {
+  // User is User model
+  // eslint-disable-next-line no-undef
+  const userData = await User.findAll().catch((err) => {
+    res.json(err);
+  });
+
+  const users = userData.map((user) => user.get({ plain: true }));
   return res.render('all', { users });
 });
 
 // get one user
 router.get('/user/:num', async (req, res) => {
-  return res.render('profile', users[req.params.num - 1]);
+  try {
+    // eslint-disable-next-line no-undef
+    const userData = await User.findbyPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    const user = userData.get({ plain: true });
+    res.render('profile', user[req.params.num - 1]);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
