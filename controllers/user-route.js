0ea6b6482525746586964536c68bc30113babca8
alexisligon <aligo@macbook-pro.home>
2021-07-07 14:12:18 -0400

    created post for new user, getting 404

--- controllers/user-route.js
+++ controllers/user-route.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-const user = require('../models/user');
+const { user } = require('../models/user');
 
 const router = require('express').Router();
 
@@ -18,8 +18,8 @@ router.get('/', async (req, res) => {
 // get one user
 router.get('/user/:num', async (req, res) => {
   try {
-    // eslint-disable-next-line no-undef
-    const userData = await User.findbyPk(req.params.id);
+    // eslint-disable-next-line no-use-before-define
+    const userData = await user.findbyPk(req.params.id);
     if (!userData) {
       res.status(404).json({ message: 'No user with this id!' });
       return;
@@ -28,6 +28,24 @@ router.get('/user/:num', async (req, res) => {
     res.render('profile', user[req.params.num - 1]);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// create a user using async/await
+router.post('/', async (req, res) => {
+  try {
+    const userData = await user.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      contact: req.body.contact,
+      isAdmin: req.body.isAdmin
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
