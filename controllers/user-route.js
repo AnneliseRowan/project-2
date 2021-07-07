f5a1670f6aaf3ef52164749d4ff6efdab7280d9f
alexisligon <aligo@macbook-pro.home>
2021-07-07 14:24:02 -0400

    delete user by id, need to test if works with seeded data

--- controllers/user-route.js
+++ controllers/user-route.js
@@ -10,6 +10,7 @@ router.get('/', async (req, res) => {
   const userData = await User.findAll().catch((err) => {
     res.json(err);
     // include other models????
+    // such as userLibrary
   });
 
   const users = userData.map((user) => user.get({ plain: true }));
@@ -17,7 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one user
-router.get('/user/:num', async (req, res) => {
+router.get('/user/:id', async (req, res) => {
   try {
     // eslint-disable-next-line no-use-before-define
     const userData = await user.findbyPk(req.params.id);
@@ -50,4 +51,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+// add put for update
+// what would we update????
+// update username, name, email, or password???
+
+// delete a user by id
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const userData = await user.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!userData) {
+    // instead of json message, possibly render page or modal popup with delete notification?
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+// router.delete('/:id', async (req, res) => {
+//     // delete a category by its `id` value
+//     try {
+//       const categoryData = await Category.destroy({
+//         where: {
+//           id: req.params.id
+//         }
+//       });
+//       if (!categoryData) {
+//         res.status(404).json({ message: 'No category found with this id!' });
+//         return;
+//       }
+//       res.status(200).json(categoryData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 module.exports = router;
