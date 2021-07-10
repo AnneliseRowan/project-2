7c6dc648437d1c019855e7897b192219fa162edb
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-07-10 14:33:49 -0400

    userData password is now 1234 by default, association when a user is deleted userlibrary udates

--- models/user.js
+++ models/user.js
@@ -60,6 +60,14 @@ module.exports = function (sequelize, DataTypes) {
     });
   };
 
+  User.associate = function (models) {
+    User.hasMany(models.UserLibrary, {
+      targetKey: 'user_id',
+      onDelete: 'CASCADE',
+      hooks: true
+    });
+  };
+
   // This will check if an unhashed password can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
