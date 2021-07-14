bd1abfc08be94ca9784732290b625cc541df390e
Lex <79127368+alexisligon@users.noreply.github.com>
2021-07-13 22:49:49 -0400

    Lex (#49)
    * created post for new user, getting 404
    
    * added a comment, possibly include other models when getting user information
    
    * delete user by id, need to test if works with seeded data
    
    * seeded and server is running
    
    * get all users function working with json message
    
    * all users in database displaying in json
    
    * get one user by id works!
    
    * added comments
    
    * update users username works
    
    * added update password
    
    * delete user by id works!
    
    * get all books to display json
    
    * get one book by id
    
    * create a book works
    
    * leaving as is
    
    * rendering books on html
    
    * changing /allbooks styling
    
    * changed sizing of text for book cards
    
    * deleted unused code, added comment for get all books route
    
    * fixed get one user api route
    
    * added future development notes to readme
    
    * added github repo link, rough summary of application, future developments
    
    * center book cards
    
    * added a button for /allbooks
    
    * changed color of #logged in p tags and h3 tags
    
    * added back to profile button at top of page
    
    * deleted a console log in /allbooks route, added a note to future development in readme
    
    * trying to merge updated code
    
    * trying to merge again
    
    * fix this
    
    * changed nav bar for all books page
    
    * deleted about me section
    
    * deleted social media icons
    
    * getting autocomplete book data from database
    
    * working on populating database books
    
    * Update user.js
    
    Co-authored-by: Jon Nance <jonathan.w.nance@gmail.com>

--- public/assets/js/example.js
+++ public/assets/js/example.js
@@ -100,14 +100,29 @@ $submitBtn.on('click', handleFormSubmit);
 $exampleList.on('click', '.delete', handleDeleteBtnClick);
 
 // the autofill
+// added this to try and return book data for autopopulate
 
+const getBooks = (data) => {
+  return $.ajax({
+    url: 'api/books',
+    type: 'GET',
+    data: JSON.parse(data)
+  });
+};
+const getMore = () => {
+  const books = getBooks();
+  const bookArray = [];
+  books.forEach(book => {
+    bookArray.push(book.title);
+  });
+  return bookArray;
+};
 // eslint-disable-next-line new-cap
 // this works even if it is underlined lol
 
 const autoCompleteJS = new autoComplete({
   data: {
-    src: [
-      'To Kill a Mockingbird - Harper Lee',
+    src: ['To Kill a Mockingbird - Harper Lee',
       '1984 - George Orwell',
       'Pride and Prejudice - Jane Austen',
       'Animal Farm - George Orwell',
@@ -331,6 +346,7 @@ const autoCompleteJS = new autoComplete({
   placeHolder: '            Search for Book Titles and Authors!',
   resultsList: {
     element: (list, data) => {
+      console.log('are we in here?');
       const info = document.createElement('p');
       if (data.results.length > 0) {
         info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
