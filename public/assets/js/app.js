6e1567ecb63131f5f49a9ea6c364f69f61f95950
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-07-14 11:23:33 -0400

    Naomi (#54)
    * editing
    
    * editing
    
    * edited book-details.handlebars, added css for book cards, added java script for book cards-
    
    * added card
    
    * fixign css
    
    * switching some things up
    
    * deleted button
    
    * trying to fix profile issues
    
    * fixed styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -132,7 +132,17 @@ $('#login').on('click', function (event) {
 });
 
 // this is for book card
+<<<<<<< HEAD
+$(document).ready(function () {
+  document.getElementById('heart').onclick = function () {
+    document.querySelector('.fa-gratip+3.ay').style.color = '#E74C3C';
+  };
+});
+<<<<<<< HEAD
+=======
+=======
 
+<<<<<<< HEAD
 
 
 $(document).ready(function () {
@@ -142,13 +152,19 @@ $(document).ready(function () {
 });
 
 
+=======
+>>>>>>> e3648837e742a247bfff0790f191059708538fe5
 // move this to example.handlebar
 //$(document).ready(function () {
  // document.getElementById('heart').onclick = function () {
   //  document.querySelector('.fa-gratip+3.ay').style.color = '#E74C3C';
   //};
 //});
+>>>>>>> main
+<<<<<<< HEAD
 
+=======
+>>>>>>> e3648837e742a247bfff0790f191059708538fe5
 
 // JS for Dynamic Questions - Need to Fix this
 
@@ -183,4 +199,8 @@ $(document).ready(function () {
 // };
 
 // $('#surveyElement').Survey({model: survey});
+<<<<<<< HEAD
+>>>>>>> main
+=======
 
+>>>>>>> main
